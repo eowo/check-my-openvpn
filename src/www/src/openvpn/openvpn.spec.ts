32d7d7d254dae33c5948a86e546ef48576279ca0
János Babik <babik.janos@gmail.com>
2019-03-02 22:25:59 +0100

    disconnect function test was added

--- src/www/src/openvpn/openvpn.spec.ts
+++ src/www/src/openvpn/openvpn.spec.ts
@@ -20,61 +20,78 @@ describe("openVpn", () => {
     expect(openVpn()).toMatchSnapshot();
   });
 
-  it("should be able to connect and emit commands", (done) => {
-    socket.addEventListener.mockImplementation(
-      (event: string, listener: () => void) => event === "connect" && listener()
-    );
+  describe("connect()", () => {
+    it("should be able to connect and emit commands", (done) => {
+      socket.addEventListener.mockImplementation(
+        (event: string, listener: () => void) =>
+          event === "connect" && listener()
+      );
 
-    openVpn()
-      .connect("10.8.0.1", 5555)
-      .subscribe({
-        next: (commands) => {
-          expect(createConnection).toHaveBeenCalledWith({
-            host: "10.8.0.1",
-            port: 5555
-          });
-          expect(commands).toMatchSnapshot();
-        },
-        complete: () => done()
-      });
+      openVpn()
+        .connect("10.8.0.1", 5555)
+        .subscribe({
+          next: (commands) => {
+            expect(createConnection).toHaveBeenCalledWith({
+              host: "10.8.0.1",
+              port: 5555
+            });
+            expect(commands).toMatchSnapshot();
+          },
+          complete: () => done()
+        });
+    });
+
+    it("should handle error event correctly", (done) => {
+      socket.addEventListener.mockImplementation(
+        (event: string, listener: (error: Error) => void) =>
+          event === "error" && listener(new Error("Error happend"))
+      );
+
+      openVpn()
+        .connect("10.8.0.1", 5555)
+        .subscribe({
+          error: (error) => {
+            expect(error).toEqual(Error("Error happend"));
+            done();
+          }
+        });
+    });
+
+    it("should handle tiemout error", (done) => {
+      jest.useFakeTimers();
+
+      openVpn()
+        .connect("10.8.0.1", 5555)
+        .subscribe({
+          error: (error) => {
+            expect(socket.destroy).toHaveBeenCalled();
+            expect(error).toEqual(new TimeoutError());
+            done();
+          }
+        });
+
+      jest.advanceTimersByTime(3000);
+      jest.useRealTimers();
+    });
   });
 
-  it("should handle error event correctly", (done) => {
-    socket.addEventListener.mockImplementation(
-      (event: string, listener: (error: Error) => void) =>
-        event === "error" && listener(new Error("Error happend"))
-    );
+  describe("disconnect()", () => {
+    it("should call socket.end()", (done) => {
+      socket.addEventListener.mockImplementation(
+        (event: string, listener: () => void) =>
+          event === "connect" && listener()
+      );
+      socket.end.mockImplementation(() => done());
 
-    openVpn()
-      .connect("10.8.0.1", 5555)
-      .subscribe({
-        error: (error) => {
-          expect(error).toEqual(Error("Error happend"));
-          done();
-        }
-      });
-  });
-
-  it("should handle tiemout error", (done) => {
-    jest.useFakeTimers();
-
-    openVpn()
-      .connect("10.8.0.1", 5555)
-      .subscribe({
-        error: (error) => {
-          expect(socket.destroy).toHaveBeenCalled();
-          expect(error).toEqual(new TimeoutError());
-          done();
-        }
-      });
-
-    jest.advanceTimersByTime(3000);
-    jest.useRealTimers();
+      const o = openVpn();
+      o.connect("10.8.0.1", 5555).subscribe({ complete: () => o.disconnect() });
+    });
   });
 });
 
 const mockSocket = () => ({
   addEventListener: jest.fn(),
   removeEventListener: jest.fn(),
-  destroy: jest.fn()
+  destroy: jest.fn(),
+  end: jest.fn()
 });
