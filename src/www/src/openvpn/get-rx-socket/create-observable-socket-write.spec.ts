d6d73e4650fb88e83ef3b173b4f0a9aa4f1118a8
János Babik <babik.janos@gmail.com>
2019-03-04 00:00:01 +0100

    Added createObservableSocketWrite tests

---
+++ src/www/src/openvpn/get-rx-socket/create-observable-socket-write.spec.ts
@@ -0,0 +1,60 @@
+import { TimeoutError } from "rxjs";
+import { createObservableSocketWrite } from "./create-observable-socket-write";
+
+describe("createObservableSocketWrite()", () => {
+  let socket: any;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  beforeEach(() => {
+    socket = mockSocket();
+  });
+
+  it("should call complete if socket is destroyed", (done) => {
+    socket.destroyed = true;
+
+    createObservableSocketWrite(socket)("command").subscribe({
+      next: () => done.fail(),
+      complete: () => done()
+    });
+  });
+
+  it("should throw TimeoutError when data wasn't written out", (done) => {
+    createObservableSocketWrite(socket)("command").subscribe({
+      error: (error) => {
+        expect(error).toBeInstanceOf(TimeoutError);
+        done();
+      }
+    });
+  });
+
+  it("should call complete when an error occurs", (done) => {
+    socket.write.mockImplementation(
+      (cmd: string, enc: string, cb: (err: string) => void) => cb("Error")
+    );
+
+    createObservableSocketWrite(socket)("command").subscribe({
+      next: () => done.fail(),
+      complete: () => done()
+    });
+  });
+
+  it("should call next when data was written out", (done) => {
+    socket.write.mockImplementation(
+      (cmd: string, enc: string, cb: (err: string, res: boolean) => void) =>
+        cb(null, true)
+    );
+
+    createObservableSocketWrite(socket)("command").subscribe({
+      next: (sent: boolean) => expect(sent).toBeTruthy(),
+      complete: () => done()
+    });
+  });
+});
+
+const mockSocket = () => ({
+  write: jest.fn(),
+  destroyed: false
+});
--- src/www/src/openvpn/get-rx-socket/create-observable-socket-write.ts
+++ src/www/src/openvpn/get-rx-socket/create-observable-socket-write.ts
@@ -2,22 +2,18 @@ import { Socket } from "net";
 import { bindNodeCallback, Observable, of } from "rxjs";
 import { catchError, filter, mapTo, mergeMap, timeout } from "rxjs/operators";
 
+const socketWrite = (socket: Socket) => (cmd: string, cb: () => void) =>
+  socket.write(cmd, "utf8", cb);
+
 export type ObservableSocketWrite = (command: string) => Observable<boolean>;
 export const createObservableSocketWrite = (
   socket: Socket
-): ObservableSocketWrite => (command: string) => {
-  const socketWrite = (cmd: string, cb: () => void) =>
-    socket.write(cmd, "utf8", cb);
-  return new Observable<boolean>((observer) =>
-    of(socket)
-      .pipe(
-        filter(() => !socket.destroyed),
-        mergeMap(() => bindNodeCallback(socketWrite)(command)),
-        mapTo(true),
-        catchError((_) => of(false)),
-        filter((sent: boolean) => sent),
-        timeout(1000)
-      )
-      .subscribe(observer)
+): ObservableSocketWrite => (command: string) =>
+  of(socket).pipe(
+    filter(({ destroyed }: Socket) => !destroyed),
+    mergeMap((s: Socket) => bindNodeCallback(socketWrite(s))(command)),
+    mapTo(true),
+    catchError(() => of(false)),
+    filter((sent: boolean) => sent),
+    timeout(1000)
   );
-};
