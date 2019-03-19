8707af2cbc3e64ae097aef4853462cce25671319
János Babik <babik.janos@gmail.com>
2019-03-19 15:03:56 +0100

    Created assets directory

--- src/www/src/index.tsx
+++ src/www/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { render } from "react-dom";
 
 import { App } from "./components/app";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
